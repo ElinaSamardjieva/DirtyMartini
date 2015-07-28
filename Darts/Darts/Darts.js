f5fa2bfc874a69ea720d878f041581a9dfab7f7b
ElinaSamardjieva <elina.sm2@gmail.com>
2015-07-28 11:40:56 +0300

    Target image added

--- Darts/Darts/Darts.js
+++ Darts/Darts/Darts.js
@@ -9,23 +9,39 @@ var bgLayer = new Kinetic.Layer();
 // On the second layer there will be the moving circle
 var secondLayer = new Kinetic.Layer();
 
-// Adding the dartboard image on the first layer
-var boardImage = new Image();
+ //Adding the dartboard image on the first layer
+  var boardImage = new Image();
 
-boardImage.onload = function () {
-    var board = new Kinetic.Image({
-        x: 0,
-        y: 0,
-        image: boardImage,
-        width: stage.width(),
-        height: stage.height()
-    });
-    
-    bgLayer.add(board);
-    bgLayer.draw();
-}
+  boardImage.onload = function () {
+      var board = new Kinetic.Image({
+          x: 0,
+          y: 0,
+          image: boardImage,
+          width: stage.width(),
+          height: stage.height()
+      });
+      
+      bgLayer.add(board);
+      bgLayer.draw();
+  };
 
-// can be changed later. This values are for testing
+//Adding the tarrget image in the second layer
+    var targetImage = new Image();
+
+      targetImage.onload = function() {
+        var target = new Kinetic.Image({
+          x: 13,
+          y: -78,
+          width: 1000,
+          height: 760,
+          image: targetImage
+        });
+        secondLayer.add(target);
+        secondLayer.draw();
+     };
+targetImage.src = 'images/target.png';
+
+//can be changed later. This values are for testing
 var targetRadius = 250,
     targetCenterX = 500,
     targetCenterY = 300; 
