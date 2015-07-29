3060a78e063c381b567368fcb95ac7451c0e250e
Novkirishki <novkirishki@yahoo.com>
2015-07-29 17:34:22 +0300

    Fixed bug hit target animation
    Fixed bug for non ending animation

--- Darts/Darts/Shooting.js
+++ Darts/Darts/Shooting.js
@@ -7,31 +7,29 @@ var Shooting = (function () {
             arrowBackground.src = 'images/olive.png';
 			
 			// initial position is down right from target
-			var arrow = new Kinetic.Rect({
-    				x: targetCenterX + targetRadius / Math.sqrt(2),
-    				y: targetCenterY + targetRadius/ Math.sqrt(2),
-    				width: 77,
-    				height: 77,
+			var arrow = new Kinetic.Circle({
+    				x: targetCenterX + targetRadius,
+    				y: targetCenterY + targetRadius / Math.sqrt(2),
+    				radius: 70,
                     fillPatternImage: arrowBackground,
-                    fillPatternOffset: { x: 0, y: 0},
-                    fillPatternScale: {x: 0.15, y: 0.15},
+                    fillPatternOffset: { x: 250, y: 250},
+                    fillPatternScale: {x: 0.27, y: 0.27},
     			}),
-                DELTA_X = (hitPointObject.x - arrow.getX()) / 120,
-                DELTA_Y = (hitPointObject.y - arrow.getY()) / 120,
-                DELTA_SCALE = 0.99; // coeff of arrow getting smaller
+                DELTA_X = (hitPointObject.x - arrow.getX()) / 60,
+                DELTA_Y = (hitPointObject.y - arrow.getY()) / 60,
+                DELTA_SCALE = 0.975; // coeff of arrow getting smaller
 			
             secondLayer.add(arrow);
                  
             function animation() {
-                if (arrow.getX() - (hitPointObject.x - arrow.height() / 2) < 1) { // when arrow hits targetPoint
+                if (arrow.getX() - hitPointObject.x < 1) { // when arrow hits targetPoint
                     deferred.resolve(hitPointObject);
                     return;
                 }
                 else {
                     arrow.setX(arrow.getX() + DELTA_X);
                     arrow.setY(arrow.getY() + DELTA_Y);
-                    arrow.width(arrow.width() * DELTA_SCALE);
-                    arrow.height(arrow.height() * DELTA_SCALE);
+                    arrow.radius(arrow.radius() * DELTA_SCALE);
                     arrow.fillPatternScaleX(arrow.fillPatternScaleX() * DELTA_SCALE);
                     arrow.fillPatternScaleY(arrow.fillPatternScaleY() * DELTA_SCALE);
                 }
