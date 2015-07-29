1fb388c17b8e7aff80c3a747c09b52273ae7d757
ElinaSamardjieva <elina.sm2@gmail.com>
2015-07-29 19:53:28 +0300

    scoreBoard.js file added

---
+++ Darts/Darts/scoreBoard.js
@@ -0,0 +1,69 @@
+function svgDisplay(x, y, width, height, nameOnePlayer, nameTwoPlayer, playerObjectOne, playerObjectTwo){
+    var paper,
+        player1 = playerObjectOne,
+        player2 = playerObjectTwo,
+        scorePlayerOne,
+        scorePlayerTwo,
+        board,
+        verticalLine,
+        horizontalLine;
+
+    function create(){
+        paper = Raphael(x, y, width, height);
+        board = paper.rect(0,0,width,height);
+        board.attr('fill','rgba(200,200,200,0.8)');
+        var path1 = "M0 50 H" + width;
+        var path2 = "M" +width/2 + " 0 V" +height;
+        horziontalLine = paper.path(path1);
+        horziontalLine.attr('stroke','#fff');
+        verticalLine = paper.path(path2);
+        verticalLine.attr('stroke','#fff');
+        addPlayerName(nameOnePlayer, 1);
+        addPlayerName(nameTwoPlayer, 2);
+        scorePlayerOne = addPlayerScore(1);
+        scorePlayerTwo = addPlayerScore(2);
+    }
+    function update(){
+        scorePlayerOne.attr('text', player1.score);
+        scorePlayerTwo.attr('text', player2.score);
+        requestAnimationFrame(update);
+    }
+
+    function addPlayerName(name, playerNumber){
+        var name = paper.text(80 + (playerNumber-1)*width/2,25,name);
+        name.attr({ "font-size": 16, "font-family": "Arial" });
+        name.attr('stroke','#000');
+
+        return name;
+    }
+
+    function addPlayerScore(playerNumber){
+        var t=paper.text(80 + (playerNumber-1)*width/2,90,'0');
+        t.attr('text', 0);
+        t.attr({ "font-size": 20, "font-family": "Arial" });
+        t.attr('stroke', '#000');
+
+        return t;
+    }
+
+    if(player1.score == undefined){
+        throw new Error('PlayerOne does not have score!');
+    }
+
+    if(player2.score == undefined){
+        throw new Error('PlayerTwo does not have score!');
+    }
+
+    create();
+    update();
+}
+
+var playerOneName = 'John';
+var playerTwoName = 'Michael';
+var playerOneScores = {score: 12};
+var playerTwoScores = {score: 10};
+
+
+svgDisplay(1012, 8, 350, 600, playerOneName,playerTwoName, playerOneScores, playerTwoScores);
+
+
